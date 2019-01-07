ccddb0e09f3cdafdce8b0788c07fd2e072f829d8
andrewcooke89 <36635993+andrewcooke89@users.noreply.github.com>
2019-01-07 13:25:05 +0000

    added basic trending carousel

--- src/Components/Carousels/TrendingCarousel/TrendingCarouselImg/TrendingCarouselImg.js
+++ src/Components/Carousels/TrendingCarousel/TrendingCarouselImg/TrendingCarouselImg.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import trendingImage from '../../../../assets/john-moeses-bauan-713049-unsplash.jpg';
+import classes from './TrendingCarouselImg.module.css';
 
 const TrendingCarouselImg = (props) => {
     return (
-        <div>
-            
-        </div>
+        <img className={classes.TrendingCarouselImg} src={trendingImage} alt="Trending-1" />
     );
 };
 
