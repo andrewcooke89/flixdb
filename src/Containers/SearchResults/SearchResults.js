fcf0969014fef7339310acbc173245b02e12feb3
andrewcooke89 <36635993+andrewcooke89@users.noreply.github.com>
2019-01-16 10:29:42 +0000

    fixed direct search loading error and null poster path loading

--- src/Containers/SearchResults/SearchResults.js
+++ src/Containers/SearchResults/SearchResults.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import classes from './SearchResults.module.css';
 import NavBar from '../../Components/NavBar/NavBar';
 import apiKey from '../../assets/apikey';
+import CarouselItem from '../../Components/Carousels/CarouselItem/CarouselItem';
 
 class SearchResults extends Component {
 
@@ -13,6 +14,8 @@ class SearchResults extends Component {
     componentDidMount() {
         this.fetchData();
     }
+
+    
 
     fetchData = () => {
         // trim the search string replacing space with %20
@@ -27,12 +30,45 @@ class SearchResults extends Component {
 
     render() {
 
+        // function for creating list of results
+        let results;
+        
+        // this check prevents error if they go to /search directly in url
+        if(this.state.resultList && this.state.resultList.length > 0)  {
+            // loops through list and removes and items that are people or have no poster
+            const filterData = this.state.resultList.filter(result => result.poster_path !== null && result.media_type !== "person");
+            
+            // loops through list and creates carousel item for each
+            results = filterData.map(result => {
+                    return (
+                        <CarouselItem
+                            item={`http://image.tmdb.org/t/p/w300${result.poster_path}`}
+                            key={result.id}
+                            id={result.id}
+                            type={result.media_type}
+                        />
+                    )
+                
+                 
+            })
+        } else {
+            results = (
+                <p className={classes.searchResults__noResults}>Sorry, there are no results for "{this.props.searchValue}". Please check the spelling and try again.</p>
+            )
+        }
+
         
         return (
             <>
                 <NavBar />
                 <main className={classes.searchResults} >
+                    
                     <h1 className={classes.searchResults__heading}>Results for {this.props.searchValue}</h1>
+        
+                    <div className={classes.searchResults_resultsContainer}>
+                        {results}
+                    </div>
+                    
                 </main>
             </>
         );
